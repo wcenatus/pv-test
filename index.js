5fdfa61751b4fd8cf6cd929f670f833ba6d2c366
wcenatus <wcenatus@gmail.com>
2021-09-08 12:00:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ require('dotenv').config()
 
 //HELPERS
 const emailHelper = require('./helpers/emailHelper');
-const domain = process.NODE_ENV === 'prod' ? "herokuapp.com" : "mysite.localhost";
+const domain = String(process.NODE_ENV) === 'prod' ? "herokuapp.com" : "mysite.localhost";
 const app = express(),
       test = express()
       // zimmer = express(),
